c8c417be0c1ce9fc024321a0b73015983487698b
Alex Sheverdin <alex@skalelabs.com>
2019-04-27 20:24:35 +0300

    SKALE-1038 Add a new s-chain check (if a block number is growing)

--- tests/test_schains.py
+++ tests/test_schains.py
@@ -4,11 +4,14 @@ import pytest
 from web3 import Web3, HTTPProvider
 
 target = os.environ.get('TARGET')
-directory = "data/" + target + "_schains"
+DATA_DIR = "data"
+SCHAINS_DIR = target + "_schains"
+directory = os.path.join(DATA_DIR, SCHAINS_DIR)
 files = os.listdir(directory)
-# print(files)
-
+BLOCK_FILE = target + "_blocks.json"
+BLOCK_FILE_PATH = os.path.join(DATA_DIR, BLOCK_FILE)
 schains = []
+
 for file in files:
 
     with open(directory + "/" + file) as json_file:
@@ -26,8 +29,29 @@ for file in files:
 @pytest.mark.parametrize("schain", schains)
 def test(schain):
     print(schain)
+    if os.path.exists(BLOCK_FILE_PATH):
+        with open(BLOCK_FILE_PATH) as json_file:
+            blocks = json.load(json_file)
+        print(f'blocks = {blocks}')
+    else:
+        print(f'no blocks file!')
+        blocks = {}
+    blocks_obj = {}
     for addr in schain['addresses']:
         web3 = Web3(HTTPProvider(addr))
         block_number = web3.eth.blockNumber
         print(f"block number = {block_number}")
         assert block_number >= 0
+        name = schain['name']
+        blocks_obj[addr] = block_number
+        if blocks:
+            print(block_number)
+            print(blocks[name][addr])
+            is_block_growing = block_number > blocks[name][addr]
+            print(f'growing is {is_block_growing}')
+            assert is_block_growing
+    blocks[name] = blocks_obj
+    with open(BLOCK_FILE_PATH, "w") as write_file:
+        json.dump(blocks, write_file)
+
+    print(f'blocks = {blocks}')
