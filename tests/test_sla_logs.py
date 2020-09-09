f2b88e6c6374a3df4a5627b122ce82a984fa00c3
Alex Sheverdin <alex@skalelabs.com>
2020-09-09 11:27:05 +0300

    SKALE-3217 Clean code

--- tests/test_sla_logs.py
+++ tests/test_sla_logs.py
@@ -17,11 +17,9 @@ def escape_ansi(line):
 
 prefix = "paramiko://ubuntu@"
 
-
-# ips = [node for node in nodes if node not in bad_ips]
-
 skale = init_skale_with_w3_wallet()
 ips = skale.nodes.get_active_node_ips()
+# ips = [ip for ip in ips if ip not in bad_ips]
 
 testinfra_hosts = [prefix + ip_from_bytes(ip) for ip in ips]
 # print(testinfra_hosts)
@@ -31,8 +29,6 @@ testinfra_hosts = [prefix + ip_from_bytes(ip) for ip in ips]
 @pytest.mark.filterwarnings('ignore')
 def test_sla_logs(host):
     ip = host.backend.host.name
-    # print(f'ip = {ip}')
-
     err_count = 0
     err_skipped_count = 0
     # cmd = f'sudo docker logs --tail {LINES_COUNT} skale_sla 2>&1| grep "{ERROR_TEXT}" || true'
@@ -47,16 +43,11 @@ def test_sla_logs(host):
         print('Lines with errors:')
         for line in lines:
             # print(f'{line}')
-            # match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}', line)
-            # cur_log_time = datetime.strptime(match.group(), FORMAT)
-            # # print(f'extracted = {cur_log_time}')
-
             if ERROR_TEXT in line:
                 # print(f'Error in SLA on host {ip}')
                 err_count += 1
     print(f'Errors: {err_count}, Skipped: {err_skipped_count}')
-    assert err_count == 1, "Multiple init was in sla-agent for {}".format(ip)
-    # assert len(failed_conts) == 0, "There are errors in IMA logs - in {} containers: {}".format(len(failed_conts), failed_conts)
+    assert err_count == 1, "Agent restarted ({} times) for node {}".format(err_count - 1, ip)
 
 
 @pytest.mark.skip(reason="skip to save time for debug")
